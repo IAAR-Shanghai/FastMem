886a7000c789f39c545abafd2eae4e82c034dab3
Shuochen Liu <89180680+tianjingyunsheng@users.noreply.github.com>
2024-06-21 12:17:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FastMem',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+      'regex',
+      'tqdm',
+      'torch==2.2.2',
+      'torchaudio==2.2.2',
+      'torchvision==0.17.2',
+      'memory_profiler',
+      'transformers==4.40',
+      'termcolor',
+      'accelerate',
+    ],
+)
 
