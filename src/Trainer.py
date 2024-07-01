097c10eefc84e0eac6b458b95f1e9c1f56ec8796
Shuochen Liu <89180680+tianjingyunsheng@users.noreply.github.com>
2024-07-01 14:35:46 +0800

    Update Trainer.py

--- src/Trainer.py
+++ src/Trainer.py
@@ -25,6 +25,23 @@ def _is_peft_model(model):
         return isinstance(model, classes_to_check)
     return False
 
+# Adaptive KL controller
+class AdaptiveKLController:
+    """
+    Adaptive KL controller described in the paper:
+    https://arxiv.org/pdf/1909.08593.pdf
+    """
+    def __init__(self, init_kl_coef, target, horizon):
+        self.value = init_kl_coef
+        self.target = target
+        self.horizon = horizon
+
+    def update(self, current, n_steps):
+        target = self.target
+        proportional_error = np.clip(current / target - 1, -0.2, 0.2)
+        mult = 1 + proportional_error * n_steps / self.horizon
+        self.value *= mult
+
 class NewTrainer(Trainer):
     def set_profile(self, args):
         if args.profile:
